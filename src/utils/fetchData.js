9cd3501a168c3d915d2b2012ba1900a006c09dd9
Janak Devkota <itzmejanak@gmail.com>
2024-11-04 17:11:50 +0545

    Netlifty proxy setup

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,20 +1,16 @@
-// utils/fetchReports.js
-
 export async function fetchData() {
     try {
-        const res = await fetch("https://ronb.cote.ws/nos");
-        
-        // Check if the response is OK
+        const res = await fetch("/api/"); // Fetch from Netlify proxy
+
         if (!res.ok) {
             throw new Error(`HTTP error! status: ${res.status}`);
         }
 
         const data = await res.json();
-        
-        return data; // Directly return the data array as per the new format
+        return data;
 
     } catch (error) {
         console.error(error);
-        return []; // Return an empty array in case of error
+        return [];
     }
 }
